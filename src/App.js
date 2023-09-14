db73f6f15a592ea965317b8c106a8bf9ad4bbfb6
bahaa ghali <bahaaghali000@gmail.com>
2023-09-15 02:58:13 +0300

    add share links Twitter Whats app Email

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
+
+import {
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+  EmailShareButton,
+  EmailIcon,
+} from "react-share";
 
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + "“" + quote.content + "”"
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +41,33 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+        <div className="share-links">
+          <TwitterShareButton
+            url={quote.author + " once said: " + "“" + quote.content + "”"}
+          >
+            <TwitterIcon size={40} round />
+          </TwitterShareButton>
+
+          <WhatsappShareButton
+            url={quote.author + " once said: " + "“" + quote.content + "”"}
+          >
+            <WhatsappIcon size={40} round />
+          </WhatsappShareButton>
+
+          <EmailShareButton
+            url={quote.author + " once said: " + "“" + quote.content + "”"}
+          >
+            <EmailIcon size={40} round />
+          </EmailShareButton>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
